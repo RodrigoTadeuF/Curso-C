cbe51d2c854e60e12813bbf7daeee758cb02e141
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2020-05-17 12:41:29 -0300

    all files untill chapter 9

--- CodigoFonte/programa14.c
+++ CodigoFonte/programa14.c
@@ -13,9 +13,6 @@ int main() {
   numero[1][0] = 3;
   numero[1][1] = 4;
 
-	// Matriz de reais
-	float valores[5][5];
-
   for(int i = 0; i < 2; i++) {
     for(int j = 0; j < 2; j++) {
       printf("numeros[%d][%d] vale %d\n", i, j, numero[i][j]);
