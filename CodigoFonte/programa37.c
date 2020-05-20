f0ef3d42a4bfb4fac93deb91f03ef8daef27a419
RodrigoTadeuF <rodrigotfonsecas@outlook.com>
2020-05-19 21:14:14 -0300

    adding files from chapter 13

---
+++ CodigoFonte/programa37.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// Recursividade
+
+/*
+Recursividade é o ato de uma função
+chamar a si mesma.
+
+OBS: é importatenq ue a função recursiva tenha uma condição de parada.
+*/
+
+int fib(int n) {
+  if(n == 0) {
+    return 0;
+  }
+
+  if (n == 1) {
+    return 1;
+  }
+
+  return fib( n - 1) + fib(n - 2);
+}
+
+int main() {
+
+  int qtd;
+
+  printf("Informe o tamanho da sequência fibonacci: ");
+  scanf("%d", &qtd);
+
+  for(int i = 0; i < qtd; i++) {
+    printf("%d ", fib(i + 1));
+  }
+
+  return 0;
+}
